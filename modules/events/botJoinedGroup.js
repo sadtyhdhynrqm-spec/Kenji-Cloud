c5785a262165fc83abe3f7a0dd8b0040292d18f8
sadtyhdhynrqm-spec <sadtyhdhynrqm@gmail.com>
2026-02-06 18:27:45 +0200

    Update botJoinedGroup.js

--- modules/events/botJoinedGroup.js
+++ modules/events/botJoinedGroup.js
@@ -8,20 +8,25 @@ module.exports = {
   },
   onStart: async ({ api, event }) => {
     try {
+      const { Threads } = require('../../database/database');
       console.log('botJoinedGroup event triggered');
       const botID = await api.getCurrentUserID();
       const addedParticipants = event.logMessageData.addedParticipants;
 
       if (addedParticipants && addedParticipants.some(p => p.userFbId === botID)) {
         console.log('Bot has been added to the group');
+        
+        // Ensure thread is created in database
+        await Threads.create(event.threadID, "New Group");
+        
         const botName = global.client.config.botName || 'Kenji Cloud';
         api.changeNickname(botName, event.threadID, botID, (err) => {
           if (err) {
             console.error("Failed to change bot nickname:", err);
-          } else {
-            console.log(`Bot nickname set to '${botName}' in thread ${event.threadID}`);
           }
         });
+        
+        api.sendMessage(`Connected successfully! Type ${global.client.config.prefix}help to see commands.`, event.threadID);
       }
     } catch (error) {
       console.error("Error in botJoinedGroup event:", error);
