95bc50f3997941cc73c1e97aa0a149fec2816417
sadtyhdhynrqm-spec <sadtyhdhynrqm@gmail.com>
2026-02-09 16:17:10 +0200

    Update botJoinedGroup.js

--- modules/events/botJoinedGroup.js
+++ modules/events/botJoinedGroup.js
@@ -22,7 +22,13 @@ module.exports = {
         });
         
         // إرسال رسالة ترحيبية لتأكيد العمل
-        api.sendMessage(` ${global.client.config.prefix}`, event.threadID);
+        api.sendMessage(` ${global.client.config.prefix} ❖━┄⋄┄━╃⊱ ★ ⊰╄━┄⋄┄━❖
+⌯︙تـم الاتـصال بـنجاح ✅
+
+⌯︙اســم البوت ⎆﹝ ابلين ﹞
+⌯︙استخدم البادئة ! للتحكم بالأوامر
+
+❖━┄⋄┄━╃⊱ ★ ⊰╄━┄⋄┄━❖`, event.threadID);
       }
     } catch (error) {
       console.error("Error in botJoinedGroup event:", error);
